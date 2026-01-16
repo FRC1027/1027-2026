227904ab4d70627ede3fc869a2b1eecdb20d5fc2
Killohurtzz <killohurtzz@gmail.com>
2026-01-15 20:27:04 -0500

    fix constants error
    nvm, fixed it! :)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,7 +4,7 @@
 
 package frc.robot;
 
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import edu.wpi.first.math.geometry.Translation3d;
