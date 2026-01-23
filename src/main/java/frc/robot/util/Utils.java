8922e3450af0cef334f2f5cb27bed8863c7574b5
Killohurtzz <killohurtzz@gmail.com>
2026-01-22 20:36:55 -0500

    small organizational stuff
    just making small edits to ensure consistency across files

--- src/main/java/frc/robot/util/Utils.java
+++ src/main/java/frc/robot/util/Utils.java
@@ -5,9 +5,7 @@ package frc.robot.util;
  */
 public final class Utils {
 
-  private Utils() {
-    // Prevent instantiation
-  }
+  private Utils() {} // Prevent instantiation
 
   /**
    * Applies a deadband filter to joystick values to ignore small inputs.
@@ -18,17 +16,17 @@ public final class Utils {
    */
   public static double deadbandReturn(double JoystickValue, double DeadbandCutOff) {
     double deadbandReturn;
-      if (JoystickValue<DeadbandCutOff&&JoystickValue>(DeadbandCutOff*(-1))) {
-      deadbandReturn=0; // if less than the deadband cutoff, return 0, if greater than the negative deadband cutoff, return 0
-    }
-    else {
-      deadbandReturn=(JoystickValue- // initially in one of two ranges: [DeadbandCutOff,1] or -1,-DeadBandCutOff]
-      (Math.abs(JoystickValue)/JoystickValue // 1 if JoystickValue > 0, -1 if JoystickValue < 0 (abs(x)/x); could use Math.signum(JoystickValue) instead
-       *DeadbandCutOff // multiply by the sign so that for >0, it comes out to - (DeadBandCutOff), and for <0 it comes to - (-DeadBandCutOff)
+
+    if (JoystickValue<DeadbandCutOff&&JoystickValue>(DeadbandCutOff*(-1))) {
+      deadbandReturn = 0; // If less than the deadband cutoff, return 0, if greater than the negative deadband cutoff, return 0
+    } else {
+      deadbandReturn = (JoystickValue - // initially in one of two ranges: [DeadbandCutOff,1] or -1,-DeadBandCutOff]
+      (Math.abs(JoystickValue) / JoystickValue // 1 if JoystickValue > 0, -1 if JoystickValue < 0 (abs(x)/x); could use Math.signum(JoystickValue) instead
+      * DeadbandCutOff // multiply by the sign so that for >0, it comes out to - (DeadBandCutOff), and for <0 it comes to - (-DeadBandCutOff)
       )
-     ) // now in either [0,1-DeadBandCutOff] or -1+DeadBandCutOff,0]
-     /(1-DeadbandCutOff); // scale to [0,1] or -1,0]
+      ) // now in either [0,1-DeadBandCutOff] or -1+DeadBandCutOff,0]
+      / (1 - DeadbandCutOff); // scale to [0,1] or -1,0]
     }
-      return deadbandReturn;
-    }
+    return deadbandReturn;
+  }
 }
